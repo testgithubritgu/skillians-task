f2ef4d85ac0339c4ce27ccd820fefa3573bb99c0
testgithubritgu <swapnilraut1698@gmail.com>
2025-09-18 16:23:21 +0530

    first commit

---
+++ typescript/app.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let a;
+a = 34;
+let s;
+s = 3;
+//# sourceMappingURL=app.js.map
---
+++ typescript/app.ts
@@ -0,0 +1,5 @@
+let a: number;
+a = 34;
+
+let s: string | number;
+s = 3;
